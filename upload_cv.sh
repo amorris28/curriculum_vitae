d19096fc91c6b547daee15719b2df27c18ad9ea2
Andrew Morris <andrewmorris@mailbox.org>
2023-10-07 09:38:24 -0400

    update CV 07.10.23-09:38:24

---
+++ upload_cv.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+# A script that pushes my csv and updates my files
+CWD=$(pwd)
+
+cd ~/Projects/curriculum_vitae
+
+pdflatex Morris_CV.tex
+pdflatex Morris_CV.tex
+
+cp Morris_CV.pdf ~/Projects/website/static/uploads/Morris_CV.pdf
+
+cd ~/Projects/website
+
+git pull
+
+git add .
+
+git commit -m 'update CV '$(date +%d.%m.%y-%H:%M:%S)
+
+git push
+
+cd ~/Projects/curriculum_vitae
+
+git pull
+
+git add .
+
+git commit -m 'update CV '$(date +%d.%m.%y-%H:%M:%S)
+
+git push
+
+cd $CWD
